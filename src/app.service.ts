6431bc42cece213eefdf91837cfed0141f8b5b1a
Aleksander Radziszewski <radziszewski.aleksander@gmail.com>
2022-06-29 17:11:52 +0200

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -8,22 +8,22 @@ export class AppService {
   constructor(
     @InjectModel('user') private readonly userModel: Model<UserDocument>
   ) { }
-  // creating User
+  
   async createUser(user: User): Promise<User> {
     const newUser = new this.userModel(user)
     return newUser.save()
   }
-  //reading User
+  
   async readUser() {
     return this.userModel.find({})
       .then(user => { return user })
       .catch(err => console.log(err))
   }
-  //updating User
+  
   async updateUser(id,data) {
     return this.userModel.findByIdAndUpdate(id, data, {new:true})
   }
-  // deleting User
+  
   async deleteUser(id) {
     return this.userModel.findByIdAndDelete(id)
   }
